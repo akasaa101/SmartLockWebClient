2fe24936518da46b636f01bc34b2c9d5313253f3
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2022-05-04 00:51:16 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Typography ,Grid, Box} from '@mui/material'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import {useState} from 'react'
+import Login from './components/login'
+import { createTheme } from '@material-ui/core/styles'
 
-function App() {
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: "#e4f0e2"
+    }
+  }
+});
+
+const themeDark = createTheme({
+  palette: {
+    background: {
+      default: "#222222"
+    },
+    text: {
+      primary: "#ffffff"
+    }
+  }
+});
+
+const App = () => {
+   const [light, setLight] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <MuiThemeProvider theme={light ? themeLight : themeDark}>
+      <CssBaseline />
+       {/* <Button onClick={() => setLight(prev => !prev)}>Toggle Theme</Button> */}
+       
+        <Container sx={{display: 'flex', alignItems: 'center', height: '100vh', width: '100%',flexDirection: 'center'}}>
+         <Login/>
+       </Container>
+
+    </MuiThemeProvider>
   );
 }
 
